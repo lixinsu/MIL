003842de4342a40d9f8140b0fa78af9d0116a430
lixinsu <sulixinict@gmail.com>
2018-03-21 00:04:17 +0800

    modeify execute scripts

--- run.sh
+++ run.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-TASK=quasart
+set -ex
 
+TASK=searchqa
+TOKENIZER=spacy
+THREAD=2
 
-python3 scripts/preprocess.py ${TASK}/ ${TASK}/ sample_train --tokenizer spacy  --workers 10
-python3 scripts/preprocess.py ${TASK}/ ${TASK}/ sample_val --tokenizer spacy  --workers 10
-python3 scripts/preprocess.py ${TASK}/ ${TASK}/ sample_test --tokenizer spacy  --workers 10
+python3 scripts/preprocess.py ${TASK}/ ${TASK}/ sample_train --tokenizer ${TOKENIZER}  --workers ${THREAD}
+python3 scripts/preprocess.py ${TASK}/ ${TASK}/ sample_val --tokenizer ${TOKENIZER}  --workers ${THREAD}
+python3 scripts/preprocess.py ${TASK}/ ${TASK}/ sample_test --tokenizer ${TOKENIZER}  --workers ${THREAD}
 
-python3 scripts/train.py 
+python3 scripts/train.py  --data-dir ${TASK} \
+                          --task ${TASK} \
+                          --loss bce \
+i                         --log-file ${TASK}_output.log
